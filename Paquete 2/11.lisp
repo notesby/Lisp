936695f7c21dc9219f9a8fd125803c2b0d2a3fdb
Hector Moreno <hector2_10@hotmail.com>
2018-03-06 21:39:18 -0600

    se soluciono el problema 11 del paquete 2

--- Paquete 2/11.lisp
+++ Paquete 2/11.lisp
@@ -2,3 +2,30 @@
 ;;elementos de cualquier tipo y anidada a cualquier nivel de profundidad) y, como
 ;;respuesta entregue una copia de la lista argumento en la cual se han removido las letras
 ;;vocales (tanto mayúsculas como minúsculas).
+
+(defun Filtra-vocales (lista) 
+	(let ( (result ()) (temp NIL) (lista-temp lista) ) 
+			(do ((i 0 (+ i 1)))
+				((>= i (length lista-temp)) result)
+				(if (listp (setq temp (nth i lista-temp)))
+					(setq lista-temp (append lista-temp temp) )
+					(if (not (or (eq temp #\a)
+								(eq temp #\e)
+								(eq temp #\i)
+								(eq temp #\o)
+								(eq temp #\u)
+								(eq temp #\A)
+								(eq temp #\E)
+								(eq temp #\I)
+								(eq temp #\O)
+								(eq temp #\U)
+								(eq temp 'a)
+								(eq temp 'e)
+								(eq temp 'i)
+								(eq temp 'o)
+								(eq temp 'u)) ) 
+						(setq result (cons temp result)) ) ) )
+			result ))
+
+;; (Filtra-vocales '( a b c (#\e f g) #\h (#\i j o #\p (1 2 3 #\u)) ))
+;;> (3 2 1 #\p J G F #\h C B)
