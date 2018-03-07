16ed42c53528d999db03dcb658d4072ae48c7302
Hector Moreno <hector2_10@hotmail.com>
2018-03-06 21:40:06 -0600

    se soluciono el problema 13 del paquete 2 de ejercicios

--- Paquete 2/13.lisp
+++ Paquete 2/13.lisp
@@ -2,3 +2,15 @@
 ;;cualquier tipo y anidada a cualquier nivel de profundidad) y, como respuesta entrega el
 ;;número de celdas de construcción que contiene la representación interna de la lista
 ;;argumento.
+
+(defun Celdas (lista) 
+	(let ( (result 0) (temp NIL) (lista-temp lista) ) 
+			(do ((i 0 (+ i 1)))
+				((>= i (length lista-temp)) result)
+				(if (listp (setq temp (nth i lista-temp)))
+					(setq lista-temp (append lista-temp temp) )
+					(setq result (+ result 1)) ) )
+			result ))
+
+;; (Celdas '(1 (2 4 (5 6) (7 8)) 3))
+;;> 8
